74515e14c7538ed2f8eca223ce77a889627a6e59
Danielle Turner <danielle.7.turner@gmail.com>
2022-03-08 15:27:38 -0600

    project structure

---
+++ src/main/java/ExampleClass.java
@@ -0,0 +1,10 @@
+import org.apache.commons.lang3.StringUtils;
+
+public class ExampleClass {
+
+    public static void main(String[] args) {
+
+        //  Below I call the stringutils.reverse then typed my string in the parenthesis followed by .soutv
+        System.out.println("StringUtils.reverse(\"Danny\") = " + StringUtils.reverse("Danny"));
+    }
+}
